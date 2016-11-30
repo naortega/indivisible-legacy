bf2499ac3f67c67378d6f68ce06365efa6955810
Nicolás A. Ortega <deathsbreed@themusicinnoise.net>
2016-11-30 15:15:52 +0100

    Basic prime calculator.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
+#include <vector>
+#include <csignal>
+
+static bool run;
+
+void leave(int signum);
 
 int main(void) {
 	std::cout << "Indivisible v0.1\n";
+	run = true;
+
+	signal(SIGINT, leave);
+
+	std::vector<unsigned long long> primes;
+	primes.push_back(2);
+	unsigned long long num = 2;
+
+	while(run) {
+		bool isPrime = true;
+		for(auto i : primes) {
+			if(i > num / 2) break;
+			if(num % i == 0) isPrime = false;
+		}
+		if(isPrime) {
+			primes.push_back(num);
+			std::cout << num << std::endl;
+		}
+		++num;
+	}
+
 	return 0;
+}
+
+void leave(int signum) {
+	std::cout << "Exiting (" << signum << ")\n";
+	run = false;
 }
